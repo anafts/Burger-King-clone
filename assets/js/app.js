5706bffeae3641694813c08e0294b3fdc486086a
Ana Freitas <ana.freitaas16@gmail.com>
2022-09-06 14:43:41 -0300

    script to scroll

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('.menu');
+let lastScroll = 0;
+
+document.addEventListener('scroll', function(){
+    const scroll = lastScroll - window.scrollY;
+
+    console.log(window.scrollY);
+
+    if (window.scrollY === 0) {
+        menu.classList.remove('fixed');
+    } else {
+        if (scroll > 0) {
+            menu.classList.add('fixed');
+            menu.classList.remove('nav-down');
+        } else {
+            menu.classList.remove('fixed');
+            menu.classList.add('nav-down');
+        }
+    }
+
+    lastScroll = window.scrollY;
+});
